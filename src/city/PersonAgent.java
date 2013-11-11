6b95a2cb07414143b342e3feac5ea24355ad9b9c
Gabriel Lew <lew.gabe@gmail.com>
2013-11-10 19:52:39 -0800

    Updated PersonAgent. Needs RestaurantCustomerRole to be implemented. hungerLevel hacked to 51.

--- src/city/PersonAgent.java
+++ src/city/PersonAgent.java
@@ -1,52 +1,91 @@
 ﻿package city;
 
+import java.awt.Point;
 import java.sql.Time;
 import java.util.*;
 
+import restaurant.HostAgent;
 import agent.Agent;
 import city.roles.*;
 
-public class PersonAgent extends Agent
-{
+public class PersonAgent extends Agent {
 	private List<Role> roles = new ArrayList<Role>();
 	//hacked in upon creation
-	//private List<MyRestaurant> restaurants = new ArrayList<Restaurant>(); 
+	private List<MyRestaurant> restaurants = new ArrayList<MyRestaurant>(); 
 	
-	
-	        enum state { };
-	        enum event { };
+	enum State { doingNothing, eating, working, shopping, banking, onWorkBreak, offWorkBreak };
+	enum Event { none, goingToEatAtHome, goingToWork, goingOutToEat };
 	private String name;
-	        Time currentTime;
-	        int hungerLevel = 0;
-	public PersonAgent(String name)
-	{
-	        this.name = name;
+	private State state = State.doingNothing; 
+	private Event event = Event.none;
+	Time currentTime;
+	int hungerLevel = 51;
+	double cashOnHand;
+	
+	class MyRestaurant {
+		//Restaurant r; 
+		HostAgent h;
+		Point location;
+		String type;
+		String name;
+		MyRestaurant(HostAgent h, Point location, String type, String name) {
+			this.h = h;
+			this.location = location;
+			this.type = type;
+			this.name = name;
+		}
 	}
+	        
+	public PersonAgent(String name) {
+	    this.name = name;
+	}
+	
 	public void addRole(Role r) {
-	        roles.add(r);
-	        r.setPerson(this);
+        roles.add(r);
+        r.setPerson(this);
 	}
+	
+	public void addRestaurant(HostAgent h, Point location, String type, String name) {
+		restaurants.add(new MyRestaurant(h, location, type, name));
+	}
+	
 	public void setName(String name){
-	        this.name = name;
+        this.name = name;
 	}
-	        public String getName(){
-	                return name;
+	
+	public String getName(){
+       return name;
 	}
-	        public boolean pickAndExecuteAnAction()
-	{
-	        try
-	{
-	//~NOTE~ only one role should be active at a time
-	        for(Role r : roles)
-	        {
-	if( r.isActive() )
-	{
-	        r.pickAndExecuteAnAction();
-	        return true;
+	
+    public boolean pickAndExecuteAnAction() {
+    	if(hungerLevel > 50 && state == State.doingNothing) {
+    		goEatFood();
+    	}
+    	
+        try {
+        	if(state == State.eating && event == Event.goingOutToEat) {
+        		goToRestaurant();
+        	}
+	        for(Role r : roles) {
+	        	if( r.isActive() ) {
+	        		r.pickAndExecuteAnAction();
+	        		return true;
+	        	}
+	        }
+	        return false;
+        } catch(ConcurrentModificationException e){ return false; }
 	}
-	}
-	return false;
-	}
-	catch(ConcurrentModificationException e){ return false; }
-	}
+    
+    private void goEatFood() {
+    	state = State.eating;
+    	event = Event.goingOutToEat;
+    }
+    
+    private void goToRestaurant() {
+    	// DoGoToRestaurant animation
+    	MyRestaurant mr = restaurants.get(0); // hack for first restaurant for now
+    	//RestaurantCustomerRole rcr = roles.get(0); // hack 
+    	//mr.h.msgIWantToEat(rcr);
+    }
+    
 }
