f28a3828d98973eab55d751cf16b56d56e4dee27
Chad Martin <chadmart@usc.edu>
2013-11-10 18:09:45 -0800

    Add personAgent and roles base class

---
+++ src/city/PersonAgent.java
@@ -0,0 +1,46 @@
+﻿package city;
+
+public class PersonAgent
+{
+	private List<Role> roles = new ArrayList<Role>();
+	//hacked in upon creation
+	private List<Restaurant> restaurants = new ArrayList<Restaurant>(); 
+	
+	
+	        enum state { };
+	        enum event { };
+	private String name;
+	        Time currentTime;
+	        int hungerLevel = 0;
+	public PersonAgent(String name)
+	{
+	        this.name = name;
+	}
+	public void addRole(Role r) {
+	        roles.add(r);
+	        r.setPerson(this);
+	}
+	public void setName(String name){
+	        this.name = name;
+	}
+	        public String getName(){
+	                return name;
+	}
+	        public boolean pickAndExecuteAnAction()
+	{
+	        try
+	{
+	//~NOTE~ only one role should be active at a time
+	        for(Role r : roles)
+	        {
+	if( r.isActive() )
+	{
+	        r.PickAndExecuteAnAction();
+	        return true;
+	}
+	}
+	return false;
+	}
+	catch(CurrentModificationException e){ return false; }
+	}
+}
---
+++ src/city/roles/Role.java
@@ -0,0 +1,26 @@
+﻿package city.roles;
+
+public abstract class Role 
+{
+	PersonAgent myPerson; 
+	public boolean active = false;
+	public void setPerson(PersonAgent a) {myPerson=a;} 
+	public PersonAgent getPersonAgent() { return myPerson;} //so other agents or role 
+	
+	
+	public Role(PersonAgent p)
+	{
+	        this.myPerson = p;
+	}
+	
+	//players can send you Person messages.
+	private void stateChanged()
+	{
+		this.active = true;
+		myPerson.stateChanged();
+	}
+	public boolean isActive() {
+	        return active;
+	}
+	public abstract boolean pickAndExecuteAnAction();
+}
