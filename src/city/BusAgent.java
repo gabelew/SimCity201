ed445f919d9c658e4ef07f436e81d232b700f05a
Gerson Hernandez <gersonh@usc.edu>
2013-11-13 18:34:07 -0800

    added one messege to the BusAgent: 'msgWaitingForBus'

--- src/city/BusAgent.java
+++ src/city/BusAgent.java
@@ -1,7 +1,7 @@
 package city;
 
+import java.awt.Point;
 import java.util.*;
-
 
 import agent.Agent;
 
@@ -9,13 +9,32 @@ public class BusAgent extends Agent{
 	public List<MyBusStop> busStops = Collections.synchronizedList(new ArrayList<MyBusStop>());
 	
 	class MyBusStop{
-		
+		Point location;
+		List<MyPassenger> passengers = Collections.synchronizedList(new ArrayList<MyPassenger>());
 	}
 	
+	class MyPassenger{
+		PersonAgent person;
+		StopEvent stopEvent;
+		
+		MyPassenger(PersonAgent p, StopEvent se){
+			person = p;
+			stopEvent = se;
+		}
+	}
 	
+	enum StopEvent {pickUp, dropOff}
+	MyBusStop atStop = null;
 	
 	//Messages
 	
+	public void msgWaitingForBus(PersonAgent p, Point busStop){
+		for(MyBusStop b : busStops){
+			if(b.location == busStop){
+				b.passengers.add(new MyPassenger(p, StopEvent.pickUp));
+			}
+		}
+	}
 	
 	//Scheduler
 	
