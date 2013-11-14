40d852a747ad05ae1fa8827f6edb58cf45b67528
Gerson Hernandez <gersonh@usc.edu>
2013-11-13 18:21:08 -0800

    Bus Agent class created but not completley implimented.

---
+++ src/city/BusAgent.java
@@ -0,0 +1,39 @@
+package city;
+
+import java.util.*;
+
+
+import agent.Agent;
+
+public class BusAgent extends Agent{
+	public List<MyBusStop> busStops = Collections.synchronizedList(new ArrayList<MyBusStop>());
+	
+	class MyBusStop{
+		
+	}
+	
+	
+	
+	//Messages
+	
+	
+	//Scheduler
+	
+	protected boolean pickAndExecuteAnAction() {
+		
+		
+		
+		
+		return false;
+	}
+	
+	
+	
+	//Actions
+	
+	
+	
+	
+	
+	
+}
