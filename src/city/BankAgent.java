183e1e620ae43fed946c73317e48914c0ba7524b
Gabriel Lew <lew.gabe@gmail.com>
2013-11-15 00:20:29 -0800

    Implemented BankAgent and BankCustomerRole. closes #2, closes #6

---
+++ src/city/BankAgent.java
@@ -0,0 +1,263 @@
+package city;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import city.roles.BankCustomerRole;
+import agent.Agent;
+
+public class BankAgent extends Agent{
+	
+	class BankAccount {
+		double currentBalance = 0.0;
+		BankCustomerRole accountHolder;
+		double owed = 0.0;
+		String accountType;
+		BankAccount(BankCustomerRole bcr, double initialDeposit, String accountType) {
+			this.accountHolder = bcr;
+			this.currentBalance = initialDeposit;
+			this.accountType = accountType;
+		}
+		void deposit(double amount) {
+			currentBalance += amount;
+		}
+		double withdraw(double amount) {
+			int result = Double.compare(currentBalance, amount);
+			if(-1 == result) {
+				currentBalance = 0.0;
+				return currentBalance;
+			} else {
+				currentBalance -= amount;
+				return amount;
+			}
+		}
+	}
+	
+	class Transaction {
+		BankAccount customer;
+		BankAccount recipient;
+		double amount;
+		TransactionState ts;
+		String purpose;
+		Transaction(TransactionState ts, double amount, BankAccount customer, BankAccount recipient, String purpose) {
+			this.ts = ts;
+			this.amount = amount;
+			this.customer = customer;
+			this.recipient = recipient;
+			this.purpose = purpose;
+		}
+	}
+	
+	enum TransactionState {none, checkBalance, withdraw, deposit, transfer, loanRequested, loanPayment};
+	List<BankAccount> accounts = new ArrayList<BankAccount>();
+	List<Transaction> transactions = new ArrayList<Transaction>();
+	
+	double fundsAvailable = 50000.0;
+	final double customerLoanMax = 500;
+	final double businessLoanMax = 1000;
+
+
+	// Messages
+	/**
+	 * For BankCustomerRole to check balance.
+	 * @param bcr
+	 * @param accountType type of account
+	 */
+	public void msgCheckBalance(BankCustomerRole bcr, String accountType) {
+		BankAccount account = findBankAccount(bcr.getPersonAgent(),accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.checkBalance, 0, account, null, "balance"));
+		}
+		stateChanged();
+	}
+	
+	/**
+	 * For PersonAgent to check balance.
+	 * @param p
+	 * @param accountType type of account
+	 */
+	public void msgCheckBalance(PersonAgent p, String accountType) {
+		BankAccount account = findBankAccount(p,accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.checkBalance, 0, account, null, "pbalance"));
+		}
+		stateChanged();
+	}
+	
+	public void msgOpenAccount(BankCustomerRole bcr, double initialDeposit, String accountType) {
+		accounts.add(new BankAccount(bcr,initialDeposit,accountType));
+		stateChanged();
+	}
+	
+	public void msgDepositMoney(BankCustomerRole bcr, double amount, String accountType) {
+		BankAccount account = findBankAccount(bcr.getPersonAgent(),accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.deposit, amount, account, null, "deposit"));
+		}
+		stateChanged();
+	}
+	
+	public void msgWithdrawMoney(BankCustomerRole bcr, double amount, String accountType) {
+		BankAccount account = findBankAccount(bcr.getPersonAgent(),accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.withdraw, amount, account, null, "withdraw"));
+		}
+		stateChanged();
+	}
+	
+	public void msgRequestLoan(BankCustomerRole bcr, double amount, String accountType) {
+		BankAccount account = findBankAccount(bcr.getPersonAgent(),accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.loanRequested, amount, account, null, "requestLoan"));
+		}
+		stateChanged();
+	}
+	
+	public void msgPayLoan(BankCustomerRole bcr, double amount, String accountType) {
+		BankAccount account = findBankAccount(bcr.getPersonAgent(),accountType);
+		if(account != null) {
+			transactions.add(new Transaction(TransactionState.loanPayment, amount, account, null, "payLoan"));
+		}
+		stateChanged();
+	}
+	
+	public void msgTransferFunds(PersonAgent sender, PersonAgent recipient, double amount, String senderAccountType, String recipientAccountType, String purpose) {
+		BankAccount senderAccount = findBankAccount(sender,senderAccountType);
+		BankAccount recipientAccount = findBankAccount(recipient,recipientAccountType);
+		if(senderAccount != null && recipientAccount != null) {
+			transactions.add(new Transaction(TransactionState.transfer, amount, senderAccount, recipientAccount, purpose));
+		}
+		stateChanged();
+	}
+
+
+	// Scheduler
+	
+	public boolean pickAndExecuteAnAction() {
+		for(Transaction t : transactions) {
+			if(TransactionState.deposit == t.ts) {
+				customerDeposit(t);
+				return true;
+			}
+		}
+		for(Transaction t : transactions) {
+			if(TransactionState.withdraw == t.ts) {
+				customerWithdrawal(t);
+				return true;
+			}
+		}
+		for(Transaction t : transactions) {
+			if(TransactionState.loanPayment == t.ts) {
+				customerLoanPayment(t);
+				return true;
+			}
+		}
+		for(Transaction t : transactions) {
+			if(TransactionState.loanRequested == t.ts) {
+				customerLoanRequest(t);
+				return true;
+			}
+		}
+		for(Transaction t : transactions) {
+			if(TransactionState.transfer == t.ts) {
+				customerTransfer(t);
+				return true;
+			}
+		}
+		for(Transaction t : transactions) {
+			if(TransactionState.checkBalance == t.ts) {
+				customerBalance(t);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// Actions
+	
+	private void customerDeposit(Transaction t) {
+		t.ts = TransactionState.none;
+		t.customer.deposit(t.amount);
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	private void customerWithdrawal(Transaction t) {
+		t.ts = TransactionState.none;
+		double withdrew = t.customer.withdraw(t.amount);
+		t.customer.accountHolder.msgHereIsMoney(withdrew, t.customer.accountType, t.customer.currentBalance);
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	private void customerLoanPayment(Transaction t) {
+		t.ts = TransactionState.none;
+		fundsAvailable += t.amount;
+		t.customer.owed -= t.amount;
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	private void customerLoanRequest(Transaction t) {
+		t.ts = TransactionState.none;
+		double canLoan = 0.0;
+		if("personal".equals(t.customer.accountType)) {
+			canLoan = customerLoanMax - t.customer.owed;
+		} else {
+			canLoan = businessLoanMax - t.customer.owed;
+		}
+		int loanLimit = Double.compare(canLoan, t.amount);
+		int bankLimit = Double.compare(fundsAvailable, t.amount);
+		if(-1 == loanLimit || -1 == bankLimit) {
+			t.customer.accountHolder.msgLoanDenied(t.amount, t.customer.accountType);
+		} else{
+			t.customer.owed += t.amount;
+			fundsAvailable -= t.amount;
+			t.customer.accountHolder.msgLoanApproved(t.amount, t.customer.accountType);
+		}
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	private void customerTransfer(Transaction t) {
+		t.ts = TransactionState.none;
+		int customerAccountLimit = Double.compare(t.customer.currentBalance, t.amount);
+		if(-1 == customerAccountLimit) {
+			t.customer.accountHolder.getPersonAgent().msgTransferFailure(t.recipient.accountHolder.getPersonAgent(), t.amount, t.purpose);
+		} else{
+			t.customer.withdraw(t.amount);
+			t.recipient.deposit(t.amount);
+			t.customer.accountHolder.getPersonAgent().msgTransferCompleted(t.recipient.accountHolder.getPersonAgent(), t.amount, t.purpose);
+			t.recipient.accountHolder.getPersonAgent().msgTransferSuccessful(t.recipient.accountHolder.getPersonAgent(), t.amount, t.purpose);
+		}
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	private void customerBalance(Transaction t) {
+		t.ts = TransactionState.none;
+		if("pbalance".equals(t.purpose)) {
+			t.customer.accountHolder.getPersonAgent().msgHereIsBalance(t.customer.currentBalance, t.customer.accountType);
+		} else {
+			t.customer.accountHolder.msgHereIsBalance(t.customer.currentBalance, t.customer.accountType);
+		}
+		transactions.remove(findTransactionIndex(t));
+	}
+	
+	// Utilities
+	
+	private BankAccount findBankAccount(PersonAgent p, String accountType) {
+		BankAccount ba = null;
+		for(BankAccount b : accounts) {
+			if(p.equals(b.accountHolder) && accountType.equals(b.accountType)) {
+				ba = b;
+			}
+		}
+		return ba;
+	}
+	
+	private int findTransactionIndex(Transaction t) {
+		int transactionIndex = -1;
+		for(int i = 0; i < transactions.size(); i++) {
+			if(t.equals(transactions.get(i))) {
+				transactionIndex = i;
+			}
+		}
+		return transactionIndex;
+	}
+}
---
+++ src/city/roles/BankCustomerRole.java
@@ -0,0 +1,196 @@
+package city.roles;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import city.BankAgent;
+import city.PersonAgent;
+
+public class BankCustomerRole extends Role{
+	
+	// Data
+	class Task {
+		BankingState bs;
+		double amount;
+		String accountType;
+		Task(BankingState bs, double amount, String at) {
+			this.bs = bs;
+			this.amount = amount;
+			this.accountType = at;
+		}
+	}
+	List<Task> tasks = new ArrayList<Task>();
+	BankAgent bank;
+	enum BankingState{WantToCheckBalance, WantToOpenAccount, WantToDeposit, WantToWithdraw, 
+		WantToGetALoan, WantToPayBackLoan, CheckingBalance, OpeningAccount, Depositing, Withdrawing, 
+		RequestingALoan, PayingLoan };
+	
+	public BankCustomerRole(PersonAgent p) {
+		super(p);
+	}
+	
+	// Messages
+	public void msgIWantToCheckBalance(String accountType) {
+		tasks.add(new Task(BankingState.WantToCheckBalance, 0, accountType));
+	}
+	
+	public void msgIWantToOpenAccount(double amount, String accountType) {
+		tasks.add(new Task(BankingState.WantToOpenAccount, amount, accountType));
+	}
+	
+	public void msgIWantToWithdraw(double amount, String accountType) {
+		tasks.add(new Task(BankingState.WantToWithdraw, amount, accountType));
+	}
+	
+	public void msgIWantToGetALoan(double amount, String accountType) {
+		tasks.add(new Task(BankingState.WantToGetALoan, amount, accountType));
+	}
+	
+	public void msgIWantToPayBackLoan(double amount, String accountType) {
+		tasks.add(new Task(BankingState.WantToPayBackLoan, amount, accountType));
+	}
+	
+	public void msgHereIsMoney(double amount, String accountType, double remainingBalance) {
+		if("personal".equals(accountType)) {
+			myPerson.cashOnHand += amount;
+		} else {
+			myPerson.businessFunds += amount;
+		}
+		print("Withdraw for $" + amount + " from " + accountType + " account was successful. Remaining balance is: " + remainingBalance);
+		stateChanged();
+	}
+	
+	public void msgHereIsBalance(double balance, String accountType) {
+		print("Current balance for " + accountType + " account is: $" + balance);
+	}
+	
+	public void msgLoanDenied(double amount, String accountType) {
+		print("$" + amount + " loan for " + accountType + " account was denied.");
+	}
+	
+	public void msgLoanApproved(double amount, String accountType) {
+		if("personal".equals(accountType)) {
+			myPerson.cashOnHand += amount;
+		} else {
+			myPerson.businessFunds += amount;
+		}
+		print("$" + amount + " loan for " + accountType + " account was approved.");
+		
+	}
+	
+	// Scheduler
+	public boolean pickAndExecuteAnAction() {
+		for(Task t : tasks) {
+			if(BankingState.WantToCheckBalance.equals(t.bs)){
+				t.bs = BankingState.CheckingBalance;
+				CheckBalance(t);
+				return true;
+			}
+		}
+		for(Task t : tasks) {
+			if(BankingState.WantToOpenAccount.equals(t.bs)){
+				t.bs = BankingState.OpeningAccount;
+				OpenAccount(t);
+				return true;
+			}
+		}
+		for(Task t : tasks) {
+			if(BankingState.WantToDeposit.equals(t.bs)){
+				t.bs = BankingState.Depositing;
+				DepositMoney(t);
+				return true;
+			}
+		}
+		for(Task t : tasks) {
+			if(BankingState.WantToWithdraw.equals(t.bs)){
+				t.bs = BankingState.Withdrawing;
+				WithdrawMoney(t);
+				return true;
+			}
+		}
+		for(Task t : tasks) {
+			if(BankingState.WantToPayBackLoan.equals(t.bs)){
+				t.bs = BankingState.PayingLoan;
+				PayLoan(t);
+				return true;
+			}
+		}
+		for(Task t : tasks) {
+			if(BankingState.WantToGetALoan.equals(t.bs)){
+				t.bs = BankingState.RequestingALoan;
+				RequestLoan(t);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//Actions
+	private void CheckBalance(Task t) {
+		bank.msgCheckBalance(this, t.accountType);
+		tasks.remove(findTaskIndex(t));
+	}
+	private void OpenAccount(Task t) {
+		bank.msgOpenAccount(this, t.amount, t.accountType);
+		tasks.remove(findTaskIndex(t));
+	}
+	private void DepositMoney(Task t) {
+		int cashLimit = 0;
+		if("personal".equals(t.accountType)) {
+			cashLimit = Double.compare(myPerson.cashOnHand, t.amount);
+		} else {
+			cashLimit = Double.compare(myPerson.businessFunds, t.amount);
+		}	
+		if(-1 == cashLimit) {
+			print("Insufficient funds to deposit");
+		} else {
+			bank.msgDepositMoney(this, t.amount, t.accountType);
+			if("personal".equals(t.accountType)) {
+				myPerson.cashOnHand -= t.amount;
+			} else {
+				myPerson.businessFunds -= t.amount;
+			}
+		}
+		tasks.remove(findTaskIndex(t));
+	}
+	private void WithdrawMoney(Task t) {
+		bank.msgWithdrawMoney(this, t.amount, t.accountType);
+		tasks.remove(findTaskIndex(t));
+	}
+	private void RequestLoan(Task t) {
+		bank.msgRequestLoan(this, t.amount, t.accountType);
+		tasks.remove(findTaskIndex(t));
+	}
+	private void PayLoan(Task t) {
+		int cashLimit = 0;
+		if("personal".equals(t.accountType)) {
+			cashLimit = Double.compare(myPerson.cashOnHand, t.amount);
+		} else {
+			cashLimit = Double.compare(myPerson.businessFunds, t.amount);
+		}	
+		if(-1 == cashLimit) {
+			print("Insufficient funds to deposit");
+		} else {
+			bank.msgPayLoan(this, t.amount, t.accountType);
+			if("personal".equals(t.accountType)) {
+				myPerson.cashOnHand -= t.amount;
+			} else {
+				myPerson.businessFunds -= t.amount;
+			}
+		}
+		tasks.remove(findTaskIndex(t));
+	}
+	
+	// Utilities
+	private int findTaskIndex(Task t) {
+		int taskIndex = -1;
+		for(int i = 0; i < tasks.size(); i++) {
+			if(t.equals(tasks.get(i))) {
+				taskIndex = i;
+			}
+		}
+		return taskIndex;
+	}
+
+	
+}
