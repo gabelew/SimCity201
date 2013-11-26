623342c39698defa50c6de4455e7afbd5104ea9d
Gerson Hernandez <gersonh@usc.edu>
2013-11-26 00:55:31 -0800

    Two persons different bus stops pick up and drop off test complete.

--- src/city/test/BusTest.java
+++ src/city/test/BusTest.java
@@ -93,5 +93,61 @@ public class BusTest extends TestCase {
 			
 		
 	}
+	
+	public void testTwoPersonsDifferentStopsPickUpAndDropOff(){
+		//check preconditions
+		assertEquals("Bus should have 4 busStops in it. It doesn't.", bus.getBusStops().size() , 4);
+		assertEquals("MockPerson's log should be empty before Bus' scheduler is called. Instead, it reads: " + person.log.toString(), 0, person.log.size());
+		assertEquals("MockPerson's log should be empty before Bus' scheduler is called. Instead, it reads: " + person2.log.toString(), 0, person2.log.size());
+
+		//the person sends the bus a msg that hey are waiting at a BusStop
+		bus.msgWaitingForBus(person, new Point(67, 85));
+						
+		assertEquals("The Bus' 4th BusStop should have a persons in it. It doesn't.", bus.getBusStops().get(3).getPassengers().size() , 1);
+		
+		bus.msgWaitingForBus(person2, new Point(67, 325));
+		
+		assertEquals("The Bus' 1st BusStop should have a persons in it. It doesn't.", bus.getBusStops().get(0).getPassengers().size() , 1);
+
+		//animation sends the bus a msg when it is at the particular bustop
+		bus.msgAtStop(new Point(30, 65));
+						
+		assertTrue("Bus scheduler should return true since the gui sent it a messege that it is at the bus stop. It doesn't.", bus.pickAndExecuteAnAction());
+		assertTrue("MockPerson should have logged \"Received msgBusIsHere\" but didn't. His log reads instead: " 
+				+ person.log.getLastLoggedEvent().toString(), person.log.containsString("Recieved msgBusIsHere from the BusAgent"));
+		assertEquals("MockPerson's log should be empty before Bus' scheduler is called. Instead, it reads: " + person2.log.toString(), 0, person2.log.size());
+		assertEquals("The Bus' 4th BusStop should have no persons in it. It does.", bus.getBusStops().get(3).getPassengers().size(), 0);
+		assertEquals("The Bus' 1st BusStop should have a person in it. It doesn't.", bus.getBusStops().get(0).getPassengers().size(), 1);
+		
+		
+		//the person sends a msg to the bus when the bus has arrived and is coming aboard
+		bus.msgComingAboard(person, new Point(67,165));
+						
+		assertEquals("The Bus' 3rd BusStop should have one person in it. It doesn't", bus.getBusStops().get(2).getPassengers().size(), 1);
+						
+		//animation sends the bus a msg when it is at the particular bustop
+		bus.msgAtStop(new Point(30, 305));
+						
+		assertTrue("Bus scheduler should return true since the gui sent it a messege that it is at the bus stop. It doesn't.", bus.pickAndExecuteAnAction());
+		assertTrue("MockPerson should have logged \"Received msgBusIsHere\" but didn't. His log reads instead: " 
+				+ person2.log.getLastLoggedEvent().toString(), person2.log.containsString("Recieved msgBusIsHere from the BusAgent"));
+		assertEquals("MockPerson's log should be 1 before Bus' scheduler is called. Instead, it reads: " + person2.log.toString(), 1, person2.log.size());
+		assertEquals("The Bus' 1st BusStop should have no person in it. It does.", bus.getBusStops().get(0).getPassengers().size(), 0);
+		assertEquals("The Bus' 3rd BusStop should have one person in it. It doesn't", bus.getBusStops().get(2).getPassengers().size(), 1);
+
+		bus.msgComingAboard(person2, new Point(67, 165));
+
+		assertEquals("The Bus' 3rd BusStop should have two persons in it. It doesn't.", bus.getBusStops().get(2).getPassengers().size(), 2);
+		
+		bus.msgAtStop(new Point(30, 145));
+		
+		assertTrue("Bus scheduler should return true since the gui sent it a messege that it is at the bus stop. It doesn't.", bus.pickAndExecuteAnAction());
+		assertTrue("MockPerson should have logged \"Received msgAtYourStop\" but didn't. His log reads instead: " 
+				+ person.log.getLastLoggedEvent().toString(), person.log.containsString("Recieved msgAtYourStop from the BusAgent"));
+		assertTrue("MockPerson should have logged \"Received msgAtYourStop\" but didn't. His log reads instead: " 
+				+ person2.log.getLastLoggedEvent().toString(), person2.log.containsString("Recieved msgAtYourStop from the BusAgent"));
+		assertEquals("The Bus' 3rd BusStop should have no persons in it. It does.", bus.getBusStops().get(2).getPassengers().size(), 0);
+		
+	}
 
 }
