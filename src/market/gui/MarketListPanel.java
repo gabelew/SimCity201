826b8e9feef6cc5468f10c16dfc6072d7093af0d
EmilyJane <ebernste@usc.edu>
2013-11-25 22:31:28 -0800

    updating market panel

--- src/market/gui/MarketListPanel.java
+++ src/market/gui/MarketListPanel.java
@@ -12,25 +12,16 @@ import java.util.ArrayList;
  */
 public class MarketListPanel extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	private long lastCheckAction = 0;
 	public JScrollPane pane =
             new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
                     JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    private JPanel view = new JPanel();
     private JPanel addListItemView = new JPanel();
-    private JButton addPersonB = new JButton("Add");
-    private JButton pauseAgentsB = new JButton("Pause");
-    private JPanel buttonGroup = new JPanel();
 
     private MarketPanel marketPanel;
     private String type;
     
     private JTextField typeNameHere = new JTextField();
-    private JCheckBox setStateCB;
-    //private List<JCheckBox> stateCBList = new ArrayList<JCheckBox>();
-    //private List<ListItem> listItems = new ArrayList<ListItem>();
-    private int tableCounter = 0;
-
+  
     static final int LIST_ITEM_VIEW_GAP = 5;
     static final int LIST_ITEM_VIEW_X = 180;
     static final int LIST_ITEM_VIEW_Y = 25;
@@ -68,58 +59,7 @@ public class MarketListPanel extends JPanel implements ActionListener {
         addListItemView.add(labels,BorderLayout.LINE_END);
         
         add(addListItemView);
-       /* setStateCB = new JCheckBox();
-        setStateCB.setVisible(true);
-        setStateCB.addActionListener(this);
-        if(type == "Customers")
-        	setStateCB.setText("Hungry?");
-        else if(type == "Waiters")
-        	setStateCB.setText("Working?");
-        setStateCB.setSelected(false);
-        setStateCB.setEnabled(true);
-        addListItemView.add(setStateCB, BorderLayout.EAST);
-        
-        
-        buttonGroup.setLayout(new BoxLayout(buttonGroup, BoxLayout.X_AXIS));
-        
-        Dimension buttonGroupSize = new Dimension(GROUP_BUTTON_X, GROUP_BUTTON_Y);
-        buttonGroup.setPreferredSize(buttonGroupSize);
-        buttonGroup.setMinimumSize(buttonGroupSize);
-        buttonGroup.setMaximumSize(buttonGroupSize);      
-        
-        FontMetrics fm = addPersonB.getFontMetrics(addPersonB.getFont());
-        
-        
-        addPersonB.addActionListener(this);
-        Dimension addPersonBSize = new Dimension(fm.stringWidth("Add") + BUTTON_PADDING, GROUP_BUTTON_Y);
-        addPersonB.setPreferredSize(addPersonBSize);
-        addPersonB.setMinimumSize(addPersonBSize);
-        addPersonB.setMaximumSize(addPersonBSize);  
-        buttonGroup.add(addPersonB);
-        
-        pauseAgentsB.addActionListener(this);
-        Dimension pauseAgentsBSize = new Dimension(fm.stringWidth("Resume") + BUTTON_PADDING, GROUP_BUTTON_Y);
-        pauseAgentsB.setPreferredSize(pauseAgentsBSize);
-        pauseAgentsB.setMinimumSize(pauseAgentsBSize);
-        pauseAgentsB.setMaximumSize(pauseAgentsBSize);
-        
-        if(this.type == "Tables")
-        {
-        	buttonGroup.add(pauseAgentsB);
-        }
-        
-        add(buttonGroup);
-
-        //view.add(addCustView);
-
-        if(this.type != "Tables")
-        {
-        	add(addListItemView);
-        }
-        
-        view.setLayout(new BoxLayout((Container) view, BoxLayout.Y_AXIS));
-        pane.setViewportView(view);
-        add(pane);*/
+      
         
     }
 
