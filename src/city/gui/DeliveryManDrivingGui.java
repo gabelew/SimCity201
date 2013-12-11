260263c039693ec491ef523a0e004856a97efe2e
Chad Martin <chadmart@usc.edu>
2013-12-10 17:04:45 -0800

    delivery man moves with traffic.

--- src/city/gui/DeliveryManDrivingGui.java
+++ src/city/gui/DeliveryManDrivingGui.java
@@ -5,17 +5,20 @@ import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
-
+import java.util.Timer;
+import java.util.TimerTask;
 import javax.imageio.ImageIO;
-
 import city.gui.Gui;
+import city.gui.AnimationPanel.GridSpot;
 import city.roles.DeliveryManRole;
 
 public class DeliveryManDrivingGui implements Gui{
 
 	private DeliveryManRole role = null;
 	private boolean isPresent = false;
+	private boolean leavingBuilding = false;
+	private boolean atStopSign = false;
+	Timer timer = new Timer();
 	
 	private static BufferedImage vanRightImg = null;
 	private static BufferedImage vanBackImg = null;
@@ -48,46 +51,146 @@ public class DeliveryManDrivingGui implements Gui{
 	
 	@Override
 	public void updatePosition() {
-		if(yPos == yDestination && xPos == xDestination&&deliveryState==state.atRestaurant){
-			deliveryState=state.waiting;
-			role.msgAnimationAtRestaurant();
-			this.isPresent = false;
+		boolean canKeepMoving = true;
+
+		for(int i = 0;i< 44;i++){
+    		for(int j =0; j<4;j++){
+    			if((xPos == i*20 - 23 - 35 && yPos == 115+j*80) || (xPos ==i*20 - 23 && yPos == 115+j*80-20 && drivingDirection == DrivingDirection.down)){
+    				GridSpot gridSpot = gui.animationPanel.gridMap.get(new Point(i*20 - 23,115+j*80));
+    				canKeepMoving = gridSpot.spot.tryAcquire();
+    				if(canKeepMoving && gridSpot.stopSign==true){
+    					gridSpot.owner = role.myPerson;
+    					timer.schedule(new TimerTask() {
+    						public void run() {
+    							atStopSign=true;
+    						}
+    					}, 
+    					200);
+
+    					timer.schedule(new TimerTask() {
+    						public void run() {
+    							atStopSign = false;
+    						}
+    					}, 
+    					1200);
+    				}
+    			}
+    			
+    			if((xPos == i*20 - 23 + 35 && yPos == 115+j*80) || (xPos == i*20 - 23 && yPos == 115+j*80-20 && drivingDirection == DrivingDirection.up)){
+    				GridSpot gridSpot = gui.animationPanel.gridMap.get(new Point(i*20 - 23,115+j*80));
+    				
+    				if(gridSpot.spot.availablePermits()==0){
+    					gridSpot.owner=null;
+    					gridSpot.spot.release();
+    				}
+    			}
+    			//left semaphores i*20 - 23, 115+j*80
+    			if(i%2==0 && i<33){
+	    			//up semaphores i*20 +97, 95+j*80
+    			}
+    			if(i%2+1==2 && i<34){
+
+    				if(yPos != yDestination && xPos != xDestination && xPos +18 <=i*20 +97  && xPos +22 >=i*20 +97 && yPos+25 <=  95+j*80 && yPos+29 >=  95+j*80){
+    					GridSpot gridSpot = gui.animationPanel.gridMap.get(new Point(i*20 +97, 95+j*80));
+	    				canKeepMoving = gridSpot.spot.tryAcquire();
+	    				if(canKeepMoving){
+	    					gridSpot.owner = role.myPerson;
+	    					leavingBuilding = true;	
+	    					xPos+=20;
+	    				}
+    				}
+    				// down semaphore i*20 +97, 95+j*80
+    			}
+    		}
+    		
+    	}
+		
+		if(leavingBuilding){
+			GridSpot gridSpot = gui.animationPanel.gridMap.get(new Point(xPos,yPos-20));
+			if(gridSpot!=null){
+				if(gridSpot.spot.availablePermits()==0){
+    				leavingBuilding = false;
+					gridSpot.owner=null;
+					gridSpot.spot.release();    					
+				}
+			}	
 		}
-		else if(yPos == yDestination && xPos == xDestination&&deliveryState==state.backToMarket){
-			deliveryState=state.waiting;
-			role.msgAnimationAtMarket();
-			this.isPresent = false;
+		
+		
+		
+		
+		
+		
+		
+		if(canKeepMoving && !atStopSign){
+			if(yPos == yDestination && xPos == xDestination&&deliveryState==state.atRestaurant){
+				deliveryState=state.waiting;
+				role.msgAnimationAtRestaurant();
+				this.isPresent = false;
+			}
+			else if(yPos == yDestination && xPos == xDestination&&deliveryState==state.backToMarket){
+				deliveryState=state.waiting;
+				role.msgAnimationAtMarket();
+				this.isPresent = false;
+			}
+			else if(yPos == yDestination && xPos == xDestination){
+				deliveryState=state.waiting;
+				this.isPresent = false;
+			}
+			else if(xPos > xDestination  && (yPos - 115)%80==0 && xPos < 950){
+				drivingDirection = DrivingDirection.right;
+				xPos++;
+			}
+			else if(yPos != yDestination && xPos != xDestination && (yPos - 115)%80!=0){
+				drivingDirection = DrivingDirection.down;
+				yPos++;
+			}else if (xPos == 950){
+				drivingDirection = DrivingDirection.right;
+				xPos = -60;
+				yPos = yDestination + 46;
+			}
+			else if (yPos != yDestination &&(xPos < xDestination || (xPos < 950 && (Math.abs(yPos - yDestination) > 47 || yPos - yDestination == -33)))){
+				drivingDirection = DrivingDirection.right;
+				xPos++;
+			}
+			else if (yPos > yDestination){
+				drivingDirection = DrivingDirection.up;
+				GridSpot gridSpot = gui.animationPanel.gridMap.get(new Point(xPos+20,yPos));
+				if(gridSpot!=null){
+					if(gridSpot.spot.availablePermits()==0){
+    					gridSpot.owner=null;
+    					gridSpot.spot.release();
+    				}
+				}
+				gridSpot = gui.animationPanel.gridMap.get(new Point(xPos-20,yPos));
+				if(gridSpot!=null){
+					if(gridSpot.spot.availablePermits()==0){
+    					gridSpot.owner=null;
+    					gridSpot.spot.release();
+    				}
+				}
+				
+				yPos--;
+			}
+			else if(xPos != xDestination){
+				drivingDirection = DrivingDirection.down;
+				yPos++;
+			}
 		}
-		else if(yPos == yDestination && xPos == xDestination){
-			deliveryState=state.waiting;
-			this.isPresent = false;
-		}
-		else if(xPos > xDestination  && (yPos - 115)%80==0 && xPos < 950){
-			drivingDirection = DrivingDirection.right;
-			xPos++;
-		}
-		else if(yPos != yDestination && xPos != xDestination && (yPos - 115)%80!=0){
-			drivingDirection = DrivingDirection.down;
-			yPos++;
-		}else if (xPos == 950){
-			drivingDirection = DrivingDirection.right;
-			xPos = 0;
-			yPos = yDestination + 46;
-		}
-		else if (yPos != yDestination &&(xPos < xDestination || (xPos < 950 && (Math.abs(yPos - yDestination) > 47 || yPos - yDestination == -33)))){
-			drivingDirection = DrivingDirection.right;
-			xPos++;
-		}
-		else if (yPos > yDestination){
-			drivingDirection = DrivingDirection.up;
-			yPos--;
-		}
-		else if(xPos != xDestination){
-			drivingDirection = DrivingDirection.down;
-			yPos++;
-		}else{
-			System.out.println("car stuck");
-		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 	@Override
